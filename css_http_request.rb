c4afb04a1de4b7e5b7001c4adc3535e3e62e1f1d
Cameron Walters <cameron.walters@gmail.com>
2008-12-22 17:21:03 -0800

    Reimplement as a class

---
+++ css_http_request.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby -wKU
+require "erb"
+require "enumerator"
+
+class CssHttpRequest
+  PREFIX = "data:,".freeze
+  LENGTH = (2000 - PREFIX.size).freeze # Internet Explorer 2KB URI limit
+
+  def self.encode(str)
+    quoted = ERB::Util.url_encode(str)
+    slice_num = 0
+    output = ""
+    quoted.each_slice(LENGTH) do |chunk|
+      output += "#c%d{background:url(%s%s);}\n" % [slice_num, PREFIX, chunk]
+      slice_num += 1
+    end
+    output
+  end  
+end
+
+
+
+if __FILE__ == $PROGRAM_NAME
+  puts CssHttpRequest.encode(STDIN.read)
+end
